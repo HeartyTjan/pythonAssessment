abc4e8a072a4ee8586454d7eff44248e8142fe0e
HeartyTjan <salamitijani02@gmail.com>
2024-10-04 14:19:54 +0100

    Tasks 4

--- PhaseGateTwo/stringinteger.py
+++ PhaseGateTwo/stringinteger.py
@@ -1,5 +1,13 @@
 def add_string_integer(first_integer, second_integer):
 
+	if not isinstance(first_integer, str) and not isinstance(second_integer, str):
+
+		raise ValueError ("Invalid input. Enter a String Integer")
+
+	if not isinstance(first_integer, str) or not isinstance(second_integer, str):
+
+		raise ValueError ("Invalid input. Enter a String Integer")
+
 	first_integer_in_number = int(first_integer)
 	second_integer_in_number = int(second_integer)
 
@@ -10,4 +18,4 @@ def add_string_integer(first_integer, second_integer):
 	return total_string
 
 
-print(add_string_integer("6","3"))	
+#print(add_string_integer("6","3"))	

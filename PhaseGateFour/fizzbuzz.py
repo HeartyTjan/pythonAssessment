3407c065ea08f0385da47db93806a515b224df6d
HeartyTjan <salamitijani02@gmail.com>
2024-10-11 16:43:05 +0100

    Tasks

---
+++ PhaseGateFour/fizzbuzz.py
@@ -0,0 +1,21 @@
+for count in range(1,51):
+
+	if count % 3 == 0 and count % 5 == 0:
+		print("fizzbuzz")
+
+	elif count % 3 == 0:
+		print("Fizz")
+
+	
+	elif  count % 5 == 0:
+		print("buzz")
+
+		
+	else :
+		print(count)
+
+	
+
+
+
+
